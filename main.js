dae103b89a5326cbe3233b578c5097f6378bf0b0
SuperHeavy3d <156587814+SuperHeavy3d@users.noreply.github.com>
2024-01-14 18:25:26 +0000

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,3 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 camera.position.z = 5;
-
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-const loader = new GLTFLoader();
-
-loader.load( '"C:\Users\pasco\Downloads\Superheavy booster 3.glb"', function ( gltf ) {
-
-	scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
