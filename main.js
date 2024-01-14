3da4cb91353a0fd5cc0bb2ef6bd5fd49812e243e
JasperPascoe <jasperpascoeofficial@gmail.com>
2024-01-14 15:20:50 +0000

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,17 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const loader = new GLTFLoader();
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+loader.load( '"C:\Users\pasco\Downloads\Superheavy booster 3.glb"', function ( gltf ) {
 
-	renderer.render( scene, camera );
-}
+	scene.add( gltf.scene );
 
-animate();
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
